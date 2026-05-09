3a1b342f39e98b0d52c0c035ad4a38162b811a6c
m4ulanaash <maulanash1117@gmail.com>
2026-05-09 22:47:34 +0700

    Fix Sanity vision module and static rendering conflict

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,7 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
+
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
@@ -21,8 +21,5 @@ export default defineConfig({
   schema,
   plugins: [
     structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
   ],
 })
